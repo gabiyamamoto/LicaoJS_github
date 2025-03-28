4fd7ac680d6a4a4f6d583ba84aa8f719036dcd84
Gabi Emi <gabiemi2021@gmail.com>
2025-03-28 09:50:20 -0300

    feat(operadores-aritmeticos): adiciona soma de dois números

--- Ex1.js
+++ Ex1.js
@@ -1,6 +1,6 @@
 let num1 = 4;
 let num2 = 8;
 
-console.log (`O resultado da soma é ${num1 + num2}`);
+console.log (`O resultado da soma é ${num1 + num2} 😊`);
 
-//Gabriela Emi 28/03/25 09:28
+//Gabriela Emi 28/03/25 09:49
