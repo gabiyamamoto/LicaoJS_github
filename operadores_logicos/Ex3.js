69445c76fbaa7d82476ca7191d5a482b9992717a
gabiyamamoto <gabiemi2021@gmail.com>
2025-03-29 19:24:34 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

--- operadores_logicos/Ex3.js
+++ operadores_logicos/Ex3.js
@@ -1,7 +1,9 @@
-let num1 = 29;
+let num1 = 101;
 let num2 = 37;
 let num3 = 52;
 
 if (num1 > 100 || num2 >100 || num3 >100 ) {
-    console.log (`O(s) número(s) passa(m) de 100`);
+    console.log (`O(s) número(s) passa(m) de 100! ✅`);
+} else {
+    console.log (`Nenhum número passa de 100! ❌`);
 }
