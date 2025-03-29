af28e4d2c75f6c4fa7a4161e858c7b33132ccf33
gabiyamamoto <gabiemi2021@gmail.com>
2025-03-29 20:30:34 -0300

    feat(operadores-logicos): adiciona verificação de par e maior que 10

---
+++ operadores_logicos/Ex5.js
@@ -0,0 +1,9 @@
+let num = 4;
+
+if (num % 2 === 0 && num > 10) {
+    console.log (`O número é par e maior que 10! 👍`);
+} else if (num > 10) {
+    console.log (`O número só é par!`);
+} else {
+    console.log (`O número só é maior que 10!`);
+}
