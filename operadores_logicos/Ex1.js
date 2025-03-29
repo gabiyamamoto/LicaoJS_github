32276b753aa90bf2b48b7dc0ed213d8d250ff414
gabiyamamoto <gabiemi2021@gmail.com>
2025-03-29 20:22:11 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

--- operadores_logicos/Ex1.js
+++ operadores_logicos/Ex1.js
@@ -3,7 +3,6 @@ let num2 = 8;
 
 if (num1 && num2 > 0) {
     console.log(`Sim! Ambos os números são positivos ✅`);
-    
 } else {
     console.log (`Não!`);
 }
---
+++ operadores_logicos/Ex4.js
@@ -0,0 +1,5 @@
+let dinheiro = 40;
+let temPermissão = true;
+
+let podeComprarLivro = dinheiro > 32 && temPermissão;
+console.log (`Pode comprar o livro?`, podeComprarLivro);
