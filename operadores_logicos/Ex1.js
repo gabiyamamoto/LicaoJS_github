6f07b73409892cd4f8c4b0f84b416c67fe614400
gabiyamamoto <gabiemi2021@gmail.com>
2025-03-29 21:01:39 -0300

    feat(operadores-logicos): adiciona checagem de idade pra votar

--- operadores_logicos/Ex1.js
+++ operadores_logicos/Ex1.js
@@ -4,4 +4,4 @@ let num2 = 8;
 let AmbosPositivos = num1 && num2 > 0;
 console.log(`Ambos os números são positivos?`, AmbosPositivos);
 
-//Gabriela
+//Gabriela Emi Yamamoto
--- operadores_logicos/Ex2.js
+++ operadores_logicos/Ex2.js
@@ -2,3 +2,5 @@ let idade = 16;
 
 let podeVotar = idade >= 16 && idade <= 70;
 console.log (`Você pode votar?`, podeVotar);
+
+//Gabriela Emi Yamamoto
