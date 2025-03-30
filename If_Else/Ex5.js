fd577680f2f5bfd6021ce89a6b816a295b46bf0d
gabiyamamoto <gabiemi2021@gmail.com>
2025-03-29 22:06:09 -0300

    feat(if-else): adiciona verificação de aprovação por nota

--- If_Else/Ex5.js
+++ If_Else/Ex5.js
@@ -1,5 +1,7 @@
 let nota = 98;
 
 if (nota >= 60 && nota <= 100) {
-    console.log (`Você foi aprovado! 🤩`)
+    console.log (`Você foi aprovado! 🤩`);
+} else {
+    console.log (`Você foi reprovado! 😭`);
 }
