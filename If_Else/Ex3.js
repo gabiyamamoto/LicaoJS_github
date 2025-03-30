c8370a4480618ae65ad654d8caf54ed05c2d7cc6
gabiyamamoto <gabiemi2021@gmail.com>
2025-03-29 22:03:02 -0300

    feat(if-else): adiciona classificação etária por idade

--- If_Else/Ex3.js
+++ If_Else/Ex3.js
@@ -3,9 +3,9 @@ let idade = 20;
 if (idade <= 12) {
     console.log (`Você é uma criança!`);
 } else if (idade >= 13 && idade <= 17) {
-    console.log (`Você é um(a) adolescente(a)!`)
+    console.log (`Você é um(a) adolescente(a)!`);
 } else if (idade >= 18 && idade <= 59) {
-    console.log (`Você é um(a) adulto(a)!`)
+    console.log (`Você é um(a) adulto(a)!`);
 } else {
-    console.log (`Você é um(a) idoso(a)!`)
+    console.log (`Você é um(a) idoso(a)!`);
 }
---
+++ If_Else/Ex5.js
@@ -0,0 +1,5 @@
+let nota = 98;
+
+if (nota >= 60 && nota <= 100) {
+    console.log (`Você foi aprovado! 🤩`)
+}
