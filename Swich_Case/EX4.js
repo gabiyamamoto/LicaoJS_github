55fcd0d22db1523ef3d03fec8ff7f3c60cabaaa7
gabiyamamoto <gabiemi2021@gmail.com>
2025-04-04 20:56:44 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Swich_Case/EX4.js
@@ -0,0 +1,27 @@
+let nota = 10;
+
+switch (nota) {
+    case 10:
+    case 9:
+        console.log (`A`);
+        break;
+    case 8:
+    case 7:
+        console.log (`B`);
+        break;
+    case 6:
+    case 5:
+        console.log (`C`);
+        break;
+    case 4:
+    case 3:
+        console.log (`D`);
+        break;
+    case 2:
+    case 1:
+    case 0:
+        console.log (`E`);
+        break;
+    default:
+        console.log (`Nota inválida!`);
+}
